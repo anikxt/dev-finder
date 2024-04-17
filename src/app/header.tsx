06479a45ccbf898a5a907564a9957748ac55a520
Aniket Kanere <aniket15304@gmail.com>
2024-04-17 21:07:07 +0530

    feat: working on displaying all of the rooms on the home page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,22 +1,70 @@
 'use client';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { LogInIcon, LogOutIcon } from 'lucide-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { ModeToggle } from '../components/mode-toggle';
 import { Button } from '../components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export function Header() {
+function AccountDropdown() {
   const session = useSession();
+  const isLoggedIn = !!session.data;
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={'link'}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ?? ''} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" /> Sign Out
+          </DropdownMenuItem>
         ) : (
-          <Button onClick={() => signIn('google')}>Sign In</Button>
+          <DropdownMenuItem onClick={() => signIn('google')}>
+            <LogInIcon className="mr-2" /> Sign In
+          </DropdownMenuItem>
         )}
-        {session.data?.user?.name}
-        <ModeToggle />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="bg-gray-100 py-4 dark:bg-gray-900 container mx-auto">
+      <div className="flex justify-between items-center">
+        <Link
+          href="/"
+          className="flex gap-2 items-center text-xl hover:underline"
+        >
+          <Image
+            src="/icon.png"
+            width="60"
+            height="60"
+            alt="the application icon of a magnifying glass"
+          />
+          DevFinder
+        </Link>
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
