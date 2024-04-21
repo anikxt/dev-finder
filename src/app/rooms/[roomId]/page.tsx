1961f5580088bda6cfbcaf127fb7124ec3e838bc
Aniket Kanere <aniket15304@gmail.com>
2024-04-21 19:02:07 +0530

    feat: your room page, middleware, editing a room

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -4,9 +4,11 @@ import { GithubIcon } from 'lucide-react';
 import Link from 'next/link';
 import { DevFinderVideo } from './video-player';
 import { splitTags } from '@/lib/utils';
+import { unstable_noStore } from 'next/cache';
 
 export default async function RoomPage(props: { params: { roomId: string } }) {
   const roomId = props.params.roomId;
+  unstable_noStore();
 
   const room = await getRoom(roomId);
 
